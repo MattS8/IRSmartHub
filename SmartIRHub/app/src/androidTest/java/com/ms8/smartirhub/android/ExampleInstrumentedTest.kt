66cdb7d878b609af18ca1b4fc7755a270077099c
Matt <matthew.steinhardt@gmail.com>
2019-05-25 15:22:38 -0700

    Project Creation
    - Created project
    - Started working on splash screen/greeting page
    - Linked FirebaseAuth and Firestore

---
+++ SmartIRHub/app/src/androidTest/java/com/ms8/smartirhub/android/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.ms8.smartirhub.android
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.ms8.smartirhub.android", appContext.packageName)
+    }
+}
